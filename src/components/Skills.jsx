14f00dcde7533e550c2fb95cd0c4e523ca117c0e
Rahul Bagga <116173597+MonsieurR05@users.noreply.github.com>
2023-04-25 19:17:27 +0100

    Add files via upload

---
+++ src/components/Skills.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { skills } from "./Data";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+
+
+
+
+
+const Skills = () => {
+    return(
+        <>
+        {skills.map(({title, percentage}, index)=> {
+            return(
+                <div className="progress__box" key={index}>
+                    <div className="progress">
+                        <CircularProgressbar strokeWidth={8} text={percentage} 
+                        value={percentage}
+                        />
+                    </div>
+                    <h3 className="title">{title}</h3>
+                </div>
+            )
+        })}
+        </>
+    )
+}
+
+export default Skills;
