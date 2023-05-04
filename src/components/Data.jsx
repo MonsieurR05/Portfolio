43e6aacb06d64ebe83414975864c572ddcda18d3
MonsieurR05 <MonsieurR9@proton.me>
2023-05-04 19:39:11 +0100

    work

---
+++ src/components/Data.jsx
@@ -0,0 +1,51 @@
+ 
+ export const skills = [
+    {
+     id: 1,
+     title: 'HTML',
+     percentage: 50 ,
+    },
+
+    {
+     id: 2,
+     title: 'CSS',
+     percentage: 40,
+    },
+
+    {
+     id: 3,
+     title: 'JavaScript',
+     percentage: 55,
+    },
+
+    {
+     id: 4,
+     title: 'C#',
+     percentage: 45,
+    },
+
+    {
+     id: 5,
+     title: 'Unity',
+     percentage: 40
+    },
+
+    {
+     id: 6,
+     title: 'Node.Js',
+     percentage: 35,
+    },
+
+    {
+     id: 7,
+     title: 'MySQL',
+     percentage: 30,
+    },
+
+    {
+     id: 8,
+     title: 'Next',
+     percentage: 55,
+    },
+
+]
---
+++ src/pages/contact.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react"
+import InputField from "../components/InputField";
+import TextareaField from "../components/TextareaField";
+import emailjs from '@emailjs/browser';
+import Layout from './../components/Layout';
+import TextAnimation from "../components/TextAnimation";
+
+
+const Contact = () => {
+  const [values, setValues] = useState({
+    user_name: '',
+    user_email: '',
+    message: ''
+  });
+  const [status, setStatus] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.send('service_w0v9tz4', 'template_vqdesaj', values, 'o4DidyseC-z8KvUdn')
+      .then(response => {
+        console.log('SUCCESS!', response);
+        setValues({
+          user_name: '',
+          user_email: '',
+          message: ''
+        });
+        
+        setStatus('SUCCESS');
+      }, error => {
+        console.log('FAILED...', error);
+      });
+  }
+
+  useEffect(() => {
+    if(status === 'SUCCESS') {
+      setTimeout(() => {
+        setStatus('');
+      }, 3000);
+    }
+  }, [status]);
+
+  const handleChange = (e) => {
+    setValues(values => ({
+      ...values,
+      [e.target.name]: e.target.value
+    }))
+  }
+  return (
+    <Layout>
+       <TextAnimation
+            text="Contact Me!"
+            className="mb-16"
+            />
+    <div className="lg:mt-48 lg:mr-48 pt-6 pb-8 bg-light shadow-xl rounded p-5">
+      {status && renderAlert()}
+      <form onSubmit={handleSubmit}>
+        <h3 className="text-dark mb-7 text-xl font-semibold">Send me a message</h3>
+        <InputField value={values.user_name} handleChange={handleChange} label="Name" name="user_name" type="text" />
+        <InputField value={values.user_email} handleChange={handleChange} label="E-Mail" name="user_email" type="email"  />
+        <TextareaField value={values.message} handleChange={handleChange} label="Your Message Here" name="message" />
+
+        <button type="submit"
+          className="mt-4 bg-dark text-light rounded px-4 py-2 "
+        >Send 
+      
+        </button>
+      </form>
+    </div>
+    </Layout>
+  
+  )
+}
+
+const renderAlert = () => (
+<div className="px-4 py-3 leading-normal text-white bg-blue-100 rounded mb-5 text-center">
+  <p>your message has been sent successfully!</p>
+</div>
+
+)
+
+export default Contact;
