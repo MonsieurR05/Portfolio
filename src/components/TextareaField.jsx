7393f2505082fbf0bc651eea5ae9323f2f8cb652
Rahul Bagga <116173597+MonsieurR05@users.noreply.github.com>
2023-05-12 10:46:31 +0100

    Update TextareaField.jsx

--- src/components/TextareaField.jsx
+++ src/components/TextareaField.jsx
@@ -1,12 +1,17 @@
-
 const TextareaField = (props) => {
-    const { handleChange, label, name, value } = props;
-    return (
-      <div>
-        <label className="text-gray-500 text-sm" htmlFor={name}>{label}</label>
-        <textarea onChange={handleChange} name={name} rows="4" className="w-full border-b-2 outline-none text-dark bg-gray-100 p-2" value={value}></textarea>
-      </div>
-    )
-  }
-  
-  export default TextareaField
+  const { handleChange, label, name, value } = props;
+  return (
+    <div>
+      <label className="text-gray-500 text-sm" htmlFor={name}>{label}</label>
+      <textarea
+        onChange={handleChange}
+        name={name}
+        rows="4"
+        className="w-full border-b-2 outline-none text-dark bg-gray-100 p-2 sm:text-base md:text-lg"
+        value={value}
+      ></textarea>
+    </div>
+  )
+}
+
+export default TextareaField
