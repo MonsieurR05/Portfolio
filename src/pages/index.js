2888fcdb69b3df2ec73113fdbb990eb48bb6ae3a
Rahul Bagga <116173597+MonsieurR05@users.noreply.github.com>
2023-05-16 17:10:58 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
               <p className='my-4 font-medium text-center text-lg sm: text-sm'>
                 As a Software Developer, I'm learning and experimenting to develop new ideas.
               </p>
-              <p className='font-medium text-lg text-center sm: text-base'>Explore my abilities in the latest projects.</p>
+              <p className='font-medium text-lg text-center sm: text-sm'>Explore my abilities in the latest projects.</p>
             </div>
           </div>
         </Layout>
