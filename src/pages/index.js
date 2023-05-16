dfd9aff1051cf431b1546492690fe7a76f17a8fb
Rahul Bagga <116173597+MonsieurR05@users.noreply.github.com>
2023-05-16 17:09:02 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
             <div className='w-4/5 sm:w-1/2 mt-5 flex flex-col items-center'>
             <h1 className='my-1 text-5xl sm:text-xl md:text-5xl text-gray-500 font-semibold font-mono relative'>Rahul &#8226; &#9794;</h1>
 
-              <h1 className='my-3 text-5xl sm:text-2xl md:text-5xl text-left font-bold relative '>
+              <h1 className='my-3 text-5xl sm:text-xl md:text-5xl text-left font-bold relative '>
                 <TypeWriter
                   options={{
                     strings: ['Front-End Dev', 'Back-End Dev', 'Game Dev'],
@@ -40,7 +40,7 @@ export default function Home() {
                   }}
                 />
               </h1>
-              <p className='my-4 font-medium text-center text-lg sm: text-lg'>
+              <p className='my-4 font-medium text-center text-lg sm: text-sm'>
                 As a Software Developer, I'm learning and experimenting to develop new ideas.
               </p>
               <p className='font-medium text-lg text-center sm: text-base'>Explore my abilities in the latest projects.</p>
