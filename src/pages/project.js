c2d28120f81e481233d7aae9abcc8a2c6ab48ac5
Rahul Bagga <116173597+MonsieurR05@users.noreply.github.com>
2023-05-23 20:12:04 +0100

    Update project.js

--- src/pages/project.js
+++ src/pages/project.js
@@ -6,23 +6,20 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Game from "../../public/images/projects/GameImg.png"
+import Pokemon from "../../public/images/projects/Pokemon.png"
+import Transition from "@/components/Transition";
 import TextAnimation from "@/components/TextAnimation";
-import TransitionEffect from '@/components/Transition';
 
 
 
-const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+const BigProject = ({ type, title, summary, img, link, github }) => {
   return (
     <article
       className="w-full flex items-center justify-between relative  rounded-br-2xl
         rounded-3xl border-4 border-solid border-dark  p-12 dark:bg-dark dark:border-light
         lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4"
     >
-      <div
-        className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark
-    rounded-br-3xl xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]
-    "
-      />
+   
       <div
         className="w-1/2 overflow-hidden rounded-lg lg:w-full"
       >
@@ -49,11 +46,13 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
         <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">
           {summary}
         </p>
+
         <div className="mt-2 flex items-center">
           <Link href={github} target="_blank" className="w-10">
-            {" "}
+           
             <GithubIcon />
           </Link>
+
           <Link
             href={link}
             target="_blank"
@@ -69,29 +68,71 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
     </article>
   );
 };
+const Project = ({ title, type, img, link, github, summary}) => {
+  return (
+    <article
+      className="w-full flex flex-col items-center justify-center rounded-2xl 
+    border-4 border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4
+    "
+    >
+     
+        <Image
+          src={img}
+          alt={title}
+          className="w-full h-auto"
+        />
+     
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base">
+          {type}
 
+        </span>
+          <h2 className="my-2  relativew-full text-left text-3xl font-bold lg:text-2xl">
+            {title}
+          </h2>
 
+          <p className="my-2 font-medium relative bottom-2 text-dark dark:text-light sm:text-sm">
+          {summary}
+        </p>
+      
+        <div className="w-full flex items-center justify-between">
+        <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold
+            dark:bg-light dark:text-dark
+            sm:px-4 sm:text-base
+            "
+          >
+           Visit
+          </Link>
+
+          <Link href={github} target="_blank" className="w-8 md:w-6">
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
 
 const projects = () => {
   return (
     <>
       <Head>
-        <title>Rahul | Projects</title>
-        <meta
-          name="description"
-        />
+        <title>Rahul | Projects </title>
       </Head>
-    <TransitionEffect/>
-      <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
-        <Layout>
+      <Transition />
+      <main className="w-full mb-16 flex flex-col items-center justify-center ">
+        <Layout className="pt-16">
           <TextAnimation
             text="Have Fun While Developing!"
-            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
+            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl sm: relative top-3"
           />
 
           <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
-              <FeaturedProject
+              <BigProject
                 type="Unity Game"
                 title="Retro Asteroids Shooter"
                 summary="A Unity Game made for a college assignment which was inspired by the original game Asteroids.
@@ -101,11 +142,24 @@ const projects = () => {
                 github="https://github.com/MonsieurR05/Unity-Game"
               />
             </div>
+
+            <div className="col-span-6 sm:col-span-12 relative bottom-11">
+              <Project
+                type="HTML, JavaScript and CSS Project"
+                title="Bouncing Images"
+                summary="A combination of HTML, JS and CSS to create bouncing images with one image that will take you to a different page.
+                Project was done as an assignment for College."
+                img={Pokemon}
+                link="/Bouncing/Project.html"
+                github="https://github.com/MonsieurR05/Bouncing"
+              />
+            </div>
           </div>
         </Layout>
       </main>
     </>
   );
 };
+
 
 export default projects;
